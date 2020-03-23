d4efa89137b8309f344753317be44f7c68cceb3a
ugur ciftci <ugurprog@gmail.com>
2020-03-23 22:49:03 +0300

    question two complated
    question two complated

--- src/org/kodluyoruz/questionOne/QuestionOne.java
+++ src/org/kodluyoruz/questionOne/QuestionOne.java
@@ -1,7 +1,34 @@
 package org.kodluyoruz.questionOne;
 
+import java.util.Arrays;
+
 public class QuestionOne {
     public static void main(String[] args) {
 
+        int array[][] = fillArray(new int[5][5], 2);
+        matrixElementWrite(array);
     }
+
+    /*
+        TODO : liste sıfır ve birlerden olusturdum fakat soruyu tam anlayamadım okuyup tekrar deneyeceğim
+     */
+    public static int[][] fillArray(int array[][], int number) {
+        for (int i = 0; i < array.length; i++) {
+            // do the for in the row according to the column size
+            for (int j = 0; j < array[i].length; j++) {
+                // multiple the random by 10 and then cast to in
+                array[i][j] = ((int) (Math.random() * number));
+            }
+        }
+        return array;
+    }
+
+    public static void matrixElementWrite(int array[][]) {
+        for (int[] row : array)
+            // converting each row as string
+            // and then printing in a separate line
+            System.out.println(Arrays.toString(row));
+    }
+
+
 }
